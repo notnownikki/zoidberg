5cea122e87828c64a68aa8cca05b03031931f52d
K Jonathan Harker <k.jonathan.harker@hp.com>
2015-09-17 16:40:48 -0700

    Add --logfile argument
    Make it possible to specify a file to log to instead of stdout.

--- zoidberg/scripts.py
+++ zoidberg/scripts.py
@@ -12,6 +12,8 @@ def main():
                         help='config yaml path')
     parser.add_argument('-v', '--verbose', dest='verbose',
                         action='store_true')
+    parser.add_argument('--logfile', default=None,
+                        help='File to log to (default: stdout)')
     options = parser.parse_args()
 
     log_level = logging.INFO
@@ -19,8 +21,12 @@ def main():
     if options.verbose:
         log_level = logging.DEBUG
 
-    logging.basicConfig(
-        format='%(asctime)s %(levelname)s %(message)s', level=log_level)
+    if options.logfile:
+        logging.basicConfig(filename=options.logfile,
+            format='%(asctime)s %(levelname)s %(message)s', level=log_level)
+    else:
+        logging.basicConfig(
+            format='%(asctime)s %(levelname)s %(message)s', level=log_level)
 
     zoidbergd = zoidberg.Zoidberg(options.config_file)
     signal.signal(signal.SIGTERM, zoidbergd.handle_signal)
